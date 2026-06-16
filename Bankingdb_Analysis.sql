d1d3c22ee72213b8449746d89588d600383cdb41
siddhibakle8-eng <siddhibakle8@gmail.com>
2026-06-16 17:00:31 +0530

    SQ1 commit

---
+++ Bankingdb_Analysis.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE BankingDb;
+USE BankingDb;
+CREATE TABLE Customers (
+   CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccountCreationDate DATE
+);
+SHOW DATABASES;
+DESC Customer;
+CREATE TABLE Accounts (
+    AccountID INT PRIMARY KEY,
+    CustomerID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID) );
+    
+    CREATE TABLE Branches (
+	BranchID INT PRIMARY KEY,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+CREATE TABLE AccountBranches (
+    AccountID INT,
+    BranchID INT,
+    AssignmentDate DATE,
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID),
+    FOREIGN KEY (BranchID) REFERENCES Branches(BranchID)
+);
+
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY,
+    CustomerID INT,
+    LoanAmount DECIMAL(10,2),
+    InterestRate DECIMAL(5,2),
+    StartDate DATE,
+    EndDate DATE,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
